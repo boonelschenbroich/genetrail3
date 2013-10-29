06dd6294b4c333424aa46fa4893538639515fa89
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2013-10-29 17:20:06 +0100

    [gtcluster] Add a stub for clusterers

---
+++ libraries/cluster/src/SparseClusterer.h
@@ -0,0 +1,34 @@
+/*
+ * GeneTrail2 - An efficent library for interpreting genetic data
+ * Copyright (C) 2013 Daniel Stöckel <dstoeckel@bioinf.uni-sb.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GT2_SPARSE_CLUSTERER_H
+#define GT2_SPARSE_CLUSTERER_H
+
+#include <config.h>
+
+/**
+ * Base class stub for clustering algorithms operating on
+ * (sparse) graphs.
+ */
+class GT2_EXPORT SparseClusterer
+{
+
+};
+
+#endif // GT2_SPARSE_CLUSTERER_H
