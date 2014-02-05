6e43edae55148aec05a6d698a21cbecaf4a45541
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2014-02-05 15:45:55 +0100

    [core] Fix CMake warnings in documentation.cmake

--- cmake/documentation.cmake
+++ cmake/documentation.cmake
@@ -6,11 +6,11 @@ if (DOXYGEN_FOUND)
 	configure_file("${PROJECT_SOURCE_DIR}/doc/Doxyfile.in" "${PROJECT_BINARY_DIR}/doc/Doxyfile")
 
 	add_custom_target(doc
-		COMMAND ${CMAKE_COMMAND} -E echo "Creating html documentation";
+		COMMAND ${CMAKE_COMMAND} -E echo "Creating html documentation"
 		COMMAND ${CMAKE_COMMAND} -E remove_directory doc/html
 		COMMAND ${CMAKE_COMMAND} -E chdir doc ${DOXYGEN_EXECUTABLE} Doxyfile
-		COMMAND ${CMAKE_COMMAND} -E echo "The documentation has been successfully created.";
-		COMMAND ${CMAKE_COMMAND} -E echo "You can now open 'doc/html/index.html' in a web browser.";
+		COMMAND ${CMAKE_COMMAND} -E echo "The documentation has been successfully created."
+		COMMAND ${CMAKE_COMMAND} -E echo "You can now open 'doc/html/index.html' in a web browser."
 		COMMAND ${CMAKE_COMMAND} -E echo ""
 		COMMENT "Build the doxygen documentation"
 		VERBATIM
