9c37eb0e2acdb144583c30e1dd29da485956bc74
Tim Kehl <Tim.Kehl@bioinf.uni-sb.de>
2014-02-21 16:26:38 +0100

    FiDePaRunner added for easier testing.

---
+++ libraries/core/src/FiDePaRunner.cpp
@@ -0,0 +1,77 @@
+#include "FiDePaRunner.h"
+
+using namespace GeneTrail;
+
+std::string FiDePaRunner::convertInt(int number) const {
+    std::stringstream ss; //create a stringstream
+    ss << number; //add number to the stream
+    return ss.str(); //return a string with the contents of the stream
+}
+
+void FiDePaRunner::writeSifFiles(std::vector<std::vector<std::string> > best_paths, std::map<std::string, std::string> regulations, std::string scores) const {
+    std::vector<std::string> strs;
+    boost::split(strs, scores, boost::is_any_of("."));
+    std::cout << strs[0] << std::endl;
+    int ki = 2;
+
+    for (std::vector<std::string> path : best_paths) {
+        std::ofstream myfile;
+        std::string file = strs[0] + ".k" + convertInt(ki) + ".sif";
+        myfile.open(file.c_str());
+        ++ki;
+
+        for (unsigned int i = 0; i < path.size() - 1; ++i) {
+            auto res = regulations.find(path[i] + path[i + 1]);
+
+            if (res != regulations.end() && res->second != "") {
+                myfile << path[i] << "\t" << res->second << "\t" << path[i + 1] << std::endl;
+            } else {
+                myfile << path[i] << "\tpp\t" << path[i + 1] << std::endl;
+            }
+        }
+
+        myfile.close();
+    }
+}
+
+void FiDePaRunner::computeDeregulatedPaths(std::string kegg, std::string scores, int pathlength, bool descending, bool absolute) const {
+    GraphType graph;
+    BoostGraphParser graph_parser;
+    BoostGraphProcessor graph_processor;
+    ScoringFileParser scoring_file_parser;
+
+    Pathfinder path_finder;
+
+    graph_parser.readCytoscapeFile<GraphType>(kegg, graph);
+
+    boost::graph_traits<GraphType>::vertex_iterator vid, vid_end;
+
+    scoring_file_parser.readScoringFile(scores);
+
+    if (absolute) {
+        scoring_file_parser.sortScoringFileAbsolute();
+    } else {
+        scoring_file_parser.sortScoringFile(descending);
+    }
+
+    std::set<std::string> vertex_set = graph_processor.getVertexSet(graph);
+
+    std::vector<std::string> sorted_gene_list = scoring_file_parser.getAllInSet(vertex_set);
+
+    std::set<std::string> vertex_set_with_scores;
+
+    for (std::string s : sorted_gene_list) {
+        vertex_set_with_scores.insert(s);
+    }
+
+    graph_processor.adeptGraph(graph, vertex_set_with_scores);
+
+    std::vector<std::vector<std::string> > best_paths;
+    std::map<std::string, std::string> regulations;
+
+    pathlength = (pathlength < (signed)sorted_gene_list.size() - 1) ? pathlength : (signed)sorted_gene_list.size() - 1;
+
+    path_finder.computeDeregulatedPath(graph, sorted_gene_list, pathlength, best_paths, regulations);
+
+    writeSifFiles(best_paths, regulations, scores);
+}
---
+++ libraries/core/src/FiDePaRunner.h
@@ -0,0 +1,68 @@
+/*
+ * GeneTrail2 - An efficent library for interpreting genetic data
+ * Copyright (C) 2014 Tim Kehl <tkehl@bioinf.uni-sb.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef FIDEPA_H
+#define FIDEPA_H
+
+#include "config.h"
+
+#include "ScoringFileParser.h"
+#include "BoostGraphParser.h"
+#include "BoostGraphProcessor.h"
+#include "Pathfinder.h"
+#include "CommandLineParser.h"
+
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string.hpp>
+
+#include <string>
+#include <cstring>
+#include <tuple>
+#include <fstream>
+#include <set>
+#include <vector>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <algorithm>
+
+
+namespace GeneTrail {
+
+    class GT2_EXPORT FiDePaRunner {
+    public:
+
+        FiDePaRunner() {
+        };
+
+        ~FiDePaRunner() {
+        };
+
+        std::string convertInt(int number) const;
+
+        void writeSifFiles(std::vector<std::vector<std::string> > best_paths, std::map<std::string, std::string> regulations, std::string scores) const;
+
+        void computeDeregulatedPaths(std::string kegg, std::string scores, int pathlength, bool descending, bool absolute) const;
+    };
+}
+
+#endif
+
