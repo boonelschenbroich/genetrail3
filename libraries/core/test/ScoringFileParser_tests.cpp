850ba2e8d816650a528e29b822b413fdbb823d2b
Tim Kehl <Tim.Kehl@bioinf.uni-sb.de>
2014-02-25 10:16:24 +0100

    ScoringFileParser tests work no correctly.

--- libraries/core/test/ScoringFileParser_tests.cpp
+++ libraries/core/test/ScoringFileParser_tests.cpp
@@ -12,84 +12,48 @@
 
 using namespace GeneTrail;
 
-TEST(Parsing, sortDescending)
-{
-	ScoringFileParser parser;
-	parser.readScoringFile(TEST_DATA_PATH("test_scores.txt"));
-	std::vector<std::tuple<std::string,double> > scores = parser.getDescendinglySortedScores();
-	bool sorted = true;
-	double tmp = 0;
+TEST(Parsing, sortIncreasingly) {
+    ScoringFileParser parser;
+    parser.readScoringFile(TEST_DATA_PATH("test_scores.txt"));
+    std::vector<std::tuple<std::string, double> > scores = parser.getIncreasinglySortedScores();
+    bool sorted = true;
+    for (unsigned int i = 1; i < scores.size(); ++i) {
+        if (std::get<1>(scores[i-1]) > std::get<1>(scores[i])) {
+            sorted = false;
+            break;
+        }
+    }
 
-	for(unsigned int i=0; i<scores.size(); ++i)
-	{
-		if(i == 0)
-		{
-			tmp = std::get<1>(scores[i]);
-		}
-		else
-		{
-			if(tmp < std::get<1>(scores[i]))
-			{
-				sorted=false;
-				break;
-			}
-		}
-	}
-
-	EXPECT_TRUE(sorted);
+    EXPECT_TRUE(sorted);
 }
 
-TEST(Parsing, sortDecreasing)
-{
-	ScoringFileParser parser;
-	parser.readScoringFile(TEST_DATA_PATH("test_scores.txt"));
-	std::vector<std::tuple<std::string,double> > scores = parser.getDecreasinglySortedScores();
-	bool sorted = true;
-	double tmp = 0;
+TEST(Parsing, sortDecreasingly) {
+    ScoringFileParser parser;
+    parser.readScoringFile(TEST_DATA_PATH("test_scores.txt"));
+    std::vector<std::tuple<std::string, double> > scores = parser.getDecreasinglySortedScores();
+    bool sorted = true;
+    for (unsigned int i = 1; i < scores.size(); ++i) {
+        if (std::get<1>(scores[i-1]) < std::get<1>(scores[i])) {
+            sorted = false;
+            break;
+        }
+    }
 
-	for(unsigned int i=0; i<scores.size(); ++i)
-	{
-		if(i == 0)
-		{
-			tmp = std::get<1>(scores[i]);
-		}
-		else
-		{
-			if(tmp > std::get<1>(scores[i]))
-			{
-				sorted=false;
-				break;
-			}
-		}
-	}
-
-	EXPECT_TRUE(sorted);
+    EXPECT_TRUE(sorted);
 }
 
-TEST(Parsing, sortAbsolute)
-{
-	ScoringFileParser parser;
-	parser.readScoringFile(TEST_DATA_PATH("test_scores.txt"));
-	parser.sortScoringFileAbsolute();
-	std::vector<std::tuple<std::string,double> > scores = parser.getAbsoluteSortedScores();
-	bool sorted = true;
-	double tmp = 0;
+TEST(Parsing, sortAbsolute) {
+    ScoringFileParser parser;
+    parser.readScoringFile(TEST_DATA_PATH("test_scores.txt"));
+    parser.sortScoringFileAbsolute();
+    std::vector<std::tuple<std::string, double> > scores = parser.getAbsoluteSortedScores();
+    bool sorted = true;
+    for (unsigned int i = 1; i < scores.size(); ++i) {
+        if (std::abs(std::get<1>(scores[i-1])) < std::abs(std::get<1>(scores[i]))) {
+            sorted = false;
+            break;
+        }
+    }
 
-	for(unsigned int i=0; i<scores.size(); ++i)
-	{
-		if(i == 0)
-		{
-			tmp = std::get<1>(scores[i]);
-		}
-		else
-		{
-			if(abs(tmp) < abs(std::get<1>(scores[i])))
-			{
-				sorted=false;
-				break;
-			}
-		}
-	}
-
-	EXPECT_TRUE(sorted);
+    EXPECT_TRUE(sorted);
 }
