49cf3cdf7633c613ebeaee193a83ad33a07f9fe3
Tim Kehl <Tim.Kehl@bioinf.uni-sb.de>
2014-02-25 10:17:19 +0100

    Pathfinder_tests renamed.

--- libraries/core/test/FiDePaRunner_tests.cpp
+++ libraries/core/test/FiDePaRunner_tests.cpp
@@ -43,19 +43,19 @@ std::set<std::string> checkFile(std::string file) {
     return dups;
 }
 
-TEST(Pathfinder, length_5) {
+TEST(FiDePaRunner, length_5) {
     //Execute FiDePa
     FiDePaRunner* f = new FiDePaRunner();
-    f->computeDeregulatedPaths(TEST_DATA_PATH("pathfinder_test.sif"), TEST_DATA_PATH("pathfinder_test_scores.txt"), 5, true, true);
+    f->computeDeregulatedPaths(TEST_DATA_PATH("FiDePaRunner_test.sif"), TEST_DATA_PATH("FiDePaRunner_test_scores.txt"), 5, true, true);
 
     //K2
-    std::set<std::string> dups = checkFile(TEST_DATA_PATH("pathfinder_test_scores.k2.sif"));
+    std::set<std::string> dups = checkFile(TEST_DATA_PATH("FiDePaRunner_test_scores.k2.sif"));
     EXPECT_EQ(dups.size(), 1);
     auto it = dups.find("AppB");
     EXPECT_TRUE(it != dups.end());
 
     //K3
-    dups = checkFile(TEST_DATA_PATH("pathfinder_test_scores.k3.sif"));
+    dups = checkFile(TEST_DATA_PATH("FiDePaRunner_test_scores.k3.sif"));
     EXPECT_EQ(2, dups.size());
     it = dups.find("BppC");
     EXPECT_TRUE(it != dups.end());
@@ -63,7 +63,7 @@ TEST(Pathfinder, length_5) {
     EXPECT_TRUE(it != dups.end());
 
     //K4
-    dups = checkFile(TEST_DATA_PATH("pathfinder_test_scores.k4.sif"));
+    dups = checkFile(TEST_DATA_PATH("FiDePaRunner_test_scores.k4.sif"));
     EXPECT_EQ(3, dups.size());
     it = dups.find("BppC");
     EXPECT_TRUE(it != dups.end());
@@ -73,7 +73,7 @@ TEST(Pathfinder, length_5) {
     EXPECT_TRUE(it != dups.end());
 
     //K5
-    dups = checkFile(TEST_DATA_PATH("pathfinder_test_scores.k5.sif"));
+    dups = checkFile(TEST_DATA_PATH("FiDePaRunner_test_scores.k5.sif"));
     EXPECT_EQ(4, dups.size());
     it = dups.find("BppC");
     EXPECT_TRUE(it != dups.end());
@@ -87,19 +87,19 @@ TEST(Pathfinder, length_5) {
     delete f;
 }
 
-TEST(Pathfinder, pathLengthToHigh) {
+TEST(FiDePaRunner, pathLengthToHigh) {
     //Execute FiDePa
     FiDePaRunner* f = new FiDePaRunner();
-    f->computeDeregulatedPaths(TEST_DATA_PATH("pathfinder_test_pathLengthToHigh.sif"), TEST_DATA_PATH("pathfinder_test_pathLengthToHigh_scores.txt"), 10, true, true);
+    f->computeDeregulatedPaths(TEST_DATA_PATH("FiDePaRunner_test_pathLengthToHigh.sif"), TEST_DATA_PATH("FiDePaRunner_test_pathLengthToHigh_scores.txt"), 10, true, true);
 
     //K2
-    std::set<std::string> dups = checkFile(TEST_DATA_PATH("pathfinder_test_pathLengthToHigh_scores.k2.sif"));
+    std::set<std::string> dups = checkFile(TEST_DATA_PATH("FiDePaRunner_test_pathLengthToHigh_scores.k2.sif"));
     EXPECT_EQ(dups.size(), 1);
     auto it = dups.find("AppB");
     EXPECT_TRUE(it != dups.end());
 
     //K3
-    dups = checkFile(TEST_DATA_PATH("pathfinder_test_pathLengthToHigh_scores.k3.sif"));
+    dups = checkFile(TEST_DATA_PATH("FiDePaRunner_test_pathLengthToHigh_scores.k3.sif"));
     EXPECT_EQ(2, dups.size());
     it = dups.find("BppC");
     EXPECT_TRUE(it != dups.end());
@@ -107,7 +107,7 @@ TEST(Pathfinder, pathLengthToHigh) {
     EXPECT_TRUE(it != dups.end());
 
     //K4
-    dups = checkFile(TEST_DATA_PATH("pathfinder_test_pathLengthToHigh_scores.k4.sif"));
+    dups = checkFile(TEST_DATA_PATH("FiDePaRunner_test_pathLengthToHigh_scores.k4.sif"));
     EXPECT_EQ(3, dups.size());
     it = dups.find("BppC");
     EXPECT_TRUE(it != dups.end());
@@ -117,7 +117,7 @@ TEST(Pathfinder, pathLengthToHigh) {
     EXPECT_TRUE(it != dups.end());
 
     //K5
-    dups = checkFile(TEST_DATA_PATH("pathfinder_test_pathLengthToHigh_scores.k5.sif"));
+    dups = checkFile(TEST_DATA_PATH("FiDePaRunner_test_pathLengthToHigh_scores.k5.sif"));
     EXPECT_EQ(4, dups.size());
     it = dups.find("BppC");
     EXPECT_TRUE(it != dups.end());
@@ -131,11 +131,33 @@ TEST(Pathfinder, pathLengthToHigh) {
     delete f;
 }
 
-TEST(Pathfinder, circle) {
+TEST(FiDePaRunner, circle) {
     FiDePaRunner* f = new FiDePaRunner();
-    f->computeDeregulatedPaths(TEST_DATA_PATH("pathfinder_test_circle.sif"), TEST_DATA_PATH("pathfinder_test_circle_scores.txt"), 10, true, true);
+    f->computeDeregulatedPaths(TEST_DATA_PATH("FiDePaRunner_test_circle.sif"), TEST_DATA_PATH("FiDePaRunner_test_circle_scores.txt"), 10, true, true);
     
-    
+    //K2
+    std::set<std::string> dups = checkFile(TEST_DATA_PATH("FiDePaRunner_test_circle_scores.k2.sif"));
+    EXPECT_EQ(dups.size(), 1);
+    auto it = dups.find("AppB");
+    EXPECT_TRUE(it != dups.end());
+
+    //K3
+    dups = checkFile(TEST_DATA_PATH("FiDePaRunner_test_circle_scores.k3.sif"));
+    EXPECT_EQ(2, dups.size());
+    it = dups.find("AppB");
+    EXPECT_TRUE(it != dups.end());
+    it = dups.find("BppC");
+    EXPECT_TRUE(it != dups.end());
+
+    //K4
+    dups = checkFile(TEST_DATA_PATH("FiDePaRunner_test_circle_scores.k4.sif"));
+    EXPECT_EQ(3, dups.size());
+    it = dups.find("BppC");
+    EXPECT_TRUE(it != dups.end());
+    it = dups.find("CppD");
+    EXPECT_TRUE(it != dups.end());
+    it = dups.find("DppA");
+    EXPECT_TRUE(it != dups.end());
     
     delete f;
 }
