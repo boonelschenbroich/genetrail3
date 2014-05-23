4b75d38ff1acbc65532ca265f5af4fc3e79bcb6f
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2014-05-23 16:03:03 +0200

    Add an initial GeneTrail2Config.cmake
    This allows external packages to easily build against GeneTrail2.
    Just issue:
     cmake .. -DGeneTrail2_DIR=/path/to/build/dir

---
+++ cmake/GT2Config.cmake
@@ -0,0 +1,25 @@
+list(APPEND GENETRAIL2_LIBRARY_INCLUDE_DIRS
+	${EIGEN3_INCLUDE_DIR}
+	${Boost_INCLUDE_DIRS}
+	"${PROJECT_SOURCE_DIR}/libraries"
+	"${PROJECT_BINARY_DIR}/libraries"
+)
+
+# TODO: This can probably be changed when we switch to CMake 3.0.
+#       That version offers to automatically detect the built targets
+#       in the export directive.
+if(TARGET gtcore)
+	list(APPEND GENETRAIL2_AVAILABLE_LIBRARIES gtcore)
+endif()
+
+if(TARGET gtcluster)
+	list(APPEND GENETRAIL2_AVAILABLE_LIBRARIES gtcluster)
+endif()
+
+export(TARGETS ${GENETRAIL2_AVAILABLE_LIBRARIES}
+	FILE "${PROJECT_BINARY_DIR}/GeneTrail2Targets.cmake"
+)
+
+export(PACKAGE GeneTrail2)
+
+configure_file(cmake/GeneTrail2Config.cmake.in "${PROJECT_BINARY_DIR}/GeneTrail2Config.cmake" @ONLY)
