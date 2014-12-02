fb6acfd54371483931cead8d9bb3938f52325ead
Tim Kehl <Tim.Kehl@bioinf.uni-sb.de>
2014-12-02 10:16:09 +0100

    Minor improvements for the permutation test and removed whitespace errors.

--- libraries/genetrail2/core/PermutationTest.h
+++ libraries/genetrail2/core/PermutationTest.h
@@ -11,11 +11,12 @@
 
 namespace GeneTrail
 {
-	template<typename value_type>
-	struct TestResult{
-		TestResult(std::string n, value_type s, size_t ss)
-		:name(n),score(s),counter(0), sampleSize(ss),enriched(false)
-		{}
+	template <typename value_type> struct TestResult
+	{
+		TestResult(const std::string& n, value_type s, size_t ss)
+		    : name(n), score(s), counter(0), sampleSize(ss), enriched(false)
+		{
+		}
 
 		std::string name;
 		value_type score;
@@ -25,83 +26,83 @@ namespace GeneTrail
 
 		double computePValue(size_t permutations)
 		{
-			return ((double)counter)/((double)permutations);
+			return ((double)counter) / ((double)permutations);
 		}
 	};
 
-	template<typename value_type>
-	class GT2_EXPORT PermutationTest
+	template <typename value_type> class GT2_EXPORT PermutationTest
 	{
 		typedef std::pair<std::string, double> PValue;
 		typedef std::vector<PValue> PValues;
 		typedef std::vector<TestResult<value_type>> TestResults;
 
 		public:
+		PermutationTest() : twister_{std::random_device{}()} {}
 
-		PermutationTest() = default;
-
-		PermutationTest(TestResults tests, std::vector<value_type> scores, size_t permutations)
-		:permutations_(permutations),tests_(tests),scores_(scores)
+		PermutationTest(TestResults tests, std::vector<value_type> scores,
+		                size_t permutations)
+		    : permutations_(permutations),
+		      tests_(tests),
+		      scores_(scores),
+		      twister_{std::random_device{}()}
 		{
 			pvalues_.reserve(tests.size());
 			// Sort vector of TestResults
 			// This makes computation faster
 			sort(tests_.begin(), tests_.end(),
-			     [](const TestResult<value_type>& a, const TestResult<value_type>& b)
+			     [](const TestResult<value_type>& a,
+			        const TestResult<value_type>& b)
 			         ->bool { return a.sampleSize < b.sampleSize; });
 		}
 
-		void shuffle(){
-			std::mt19937 twister{std::random_device{}()};
-			std::shuffle(scores_.begin(), scores_.end(), twister);
+		void shuffle()
+		{
+			std::shuffle(scores_.begin(), scores_.end(), twister_);
 		}
 
-		template<typename Function>
-		PValues computePValue(Function f){
-			for(size_t i=0; i< permutations_; ++i){
-				std::cout << "INFO: Running - Permutation test " << i << "/" << permutations_ << std::endl;
+		template <typename Function> PValues computePValue(Function f)
+		{
+			for(size_t i = 0; i < permutations_; ++i) {
+				std::cout << "INFO: Running - Permutation test " << i << "/"
+				          << permutations_ << std::endl;
 				// As we don't want to recompute any values,
 				// we save them here.
 				shuffle();
 				size_t currentSampleSize = -1;
 				value_type currentScore = 0.0;
-				for(size_t i=0; i<tests_.size(); ++i){
+				for(size_t i = 0; i < tests_.size(); ++i) {
 					// Check if we need to compute new values
-					if(tests_[i].sampleSize != currentSampleSize)
-					{
+					if(tests_[i].sampleSize != currentSampleSize) {
 						currentSampleSize = tests_[i].sampleSize;
-						currentScore = f(scores_.begin(),scores_.begin() + currentSampleSize);
-						//std::cout << currentSampleSize << "\t" << currentScore  << std::endl;
+						currentScore = f(scores_.begin(),
+						                 scores_.begin() + currentSampleSize);
 					}
-					if(tests_[i].enriched)
-					{
-						if(tests_[i].score <= currentScore){
+					if(tests_[i].enriched) {
+						if(tests_[i].score <= currentScore) {
 							++tests_[i].counter;
 						}
-					}
-					else
-					{
-						if(tests_[i].score >= currentScore){
+					} else {
+						if(tests_[i].score >= currentScore) {
 							++tests_[i].counter;
 						}
 					}
 				}
 			}
-			for(size_t i=0; i<tests_.size(); ++i){
-				//std::cout << tests_[i].name << "\t" << tests_[i].computePValue(permutations_)  << std::endl;
-				pvalues_.push_back(std::make_pair(tests_[i].name, tests_[i].computePValue(permutations_)));
+			for(size_t i = 0; i < tests_.size(); ++i) {
+				pvalues_.push_back(std::make_pair(
+				    tests_[i].name, tests_[i].computePValue(permutations_)));
 			}
 			return pvalues_;
 		}
 
 		private:
-
 		size_t permutations_;
 		TestResults tests_;
 		PValues pvalues_;
 		std::vector<value_type> scores_;
+		std::mt19937 twister_;
 	};
 }
 
-#endif //GT2_CORE_PERMUTATION_TEST_H
+#endif // GT2_CORE_PERMUTATION_TEST_H
 
