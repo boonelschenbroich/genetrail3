443d5082fcc51dca5407f3f7293f5a0ba26ff63b
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2014-12-02 11:06:24 +0100

    Statistic.h: fix formatting

--- libraries/genetrail2/core/Statistic.h
+++ libraries/genetrail2/core/Statistic.h
@@ -24,101 +24,123 @@
 #include <algorithm>
 #include <vector>
 
-namespace GeneTrail {
+namespace GeneTrail
+{
 
-    /**
-     * A collection of mathematical operations.
-     */
-    namespace statistic {
+	/**
+	 * A collection of mathematical operations.
+	 */
+	namespace statistic
+	{
 
 		/**
-		 * This method calculates the absolute value for each entry of a given range.
+		 * This method calculates the absolute value for each entry of a given
+		 * range.
 		 *
 		 * @param begin InputIterator
 		 * @param end InputIterator
 		 */
 		template <typename value_type, typename InputIterator>
-		void abs(InputIterator begin, InputIterator end) {
-			std::transform (begin, end , begin, static_cast< value_type(*)(value_type) >( std::abs ));
+		void abs(InputIterator begin, InputIterator end)
+		{
+			std::transform(begin, end, begin,
+			               static_cast<value_type (*)(value_type)>(std::abs));
 		}
 
 		/**
-		 * This method calculates the square root for each entry of a given range.
+		 * This method calculates the square root for each entry of a given
+		 * range.
 		 *
 		 * @param begin InputIterator
 		 * @param end InputIterator
 		 */
 		template <typename value_type, typename InputIterator>
-		void sqrt(InputIterator begin, InputIterator end){
-			std::transform (begin, end , begin, static_cast< value_type(*)(value_type) >( std::sqrt ));
+		void sqrt(InputIterator begin, InputIterator end)
+		{
+			std::transform(begin, end, begin,
+			               static_cast<value_type (*)(value_type)>(std::sqrt));
 		}
 
 		/**
-		 * This method calculates the natural logarithm for each entry of a given range.
+		 * This method calculates the natural logarithm for each entry of a
+		 * given range.
 		 *
 		 * @param begin InputIterator
 		 * @param end InputIterator
 		 */
 		template <typename value_type, typename InputIterator>
-		void log(InputIterator begin, InputIterator end){
-			std::transform (begin, end , begin, static_cast< value_type(*)(value_type) >( std::log ));
+		void log(InputIterator begin, InputIterator end)
+		{
+			std::transform(begin, end, begin,
+			               static_cast<value_type (*)(value_type)>(std::log));
 		}
 
 		/**
-		 * This method calculates the logarithm (base 10) for each entry of a given range.
+		 * This method calculates the logarithm (base 10) for each entry of a
+		 * given range.
 		 *
 		 * @param begin InputIterator
 		 * @param end InputIterator
 		 */
 		template <typename value_type, typename InputIterator>
-		void log10(InputIterator begin, InputIterator end){
-			std::transform (begin, end , begin, static_cast< value_type(*)(value_type) >( std::log10 ));
+		void log10(InputIterator begin, InputIterator end)
+		{
+			std::transform(begin, end, begin,
+			               static_cast<value_type (*)(value_type)>(std::log10));
 		}
 
 		/**
-		 * This method calculates the logarithm (base 2) for each entry of a given range.
+		 * This method calculates the logarithm (base 2) for each entry of a
+		 * given range.
 		 *
 		 * @param begin InputIterator
 		 * @param end InputIterator
 		 */
 		template <typename value_type, typename InputIterator>
-		void log2(InputIterator begin, InputIterator end){
-			std::transform (begin, end , begin, static_cast< value_type(*)(value_type) >( std::log2 ));
+		void log2(InputIterator begin, InputIterator end)
+		{
+			std::transform(begin, end, begin,
+			               static_cast<value_type (*)(value_type)>(std::log2));
 		}
 
 		/**
-		 * This method calculates the n-th power for each entry of a given range.
+		 * This method calculates the n-th power for each entry of a given
+		 * range.
 		 * @param begin InputIterator
 		 * @param end InputIterator
 		 */
 		template <typename value_type, typename InputIterator>
-		void pow(InputIterator begin, InputIterator end, int n){
-			std::transform (begin, end , begin, [n](value_type x){ return std::pow(x,n);});
+		void pow(InputIterator begin, InputIterator end, int n)
+		{
+			std::transform(begin, end, begin,
+			               [n](value_type x) { return std::pow(x, n); });
 		}
 
-        /**
-         * This method calculates the maximal value of a given range.
-         *
-         * @param begin InputIterator
+		/**
+		 * This method calculates the maximal value of a given range.
+		 *
+		 * @param begin InputIterator
 		 * @param end InputIterator
-         * @return Maximum of the given range
-         */
-        template <typename value_type, typename InputIterator>
-        value_type max(InputIterator begin, InputIterator end) {
-            return *std::max_element(begin, end);
-        }
-
-        /**
-         * This method calculates the minimal value of a given range.
-         *
-         * @param begin InputIterator
-		 * @param end InputIterator
-         * @return Minimum of the given range
-         */
+		 * @return Maximum of the given range
+		 */
 		template <typename value_type, typename InputIterator>
-        value_type min(InputIterator begin, InputIterator end) {
-            return *std::min_element(begin, end);
-        }
+		value_type max(InputIterator begin, InputIterator end)
+		{
+			return *std::max_element(begin, end);
+		}
+
+		/**
+		 * This method calculates the minimal value of a given range.
+		 *
+		 * @param begin InputIterator
+		 * @param end InputIterator
+		 * @return Minimum of the given range
+		 */
+		template <typename value_type, typename InputIterator>
+		value_type min(InputIterator begin, InputIterator end)
+		{
+			return *std::min_element(begin, end);
+		}
 
 		/**
 		 * This method calculates the sum of a given range.
@@ -128,21 +150,24 @@ namespace GeneTrail {
 		 * @return Sum of the given range
 		 */
 		template <typename value_type, typename InputIterator>
-		value_type sum(InputIterator begin, InputIterator end) {
-			return std::accumulate(begin, end,  0.0);
+		value_type sum(InputIterator begin, InputIterator end)
+		{
+			return std::accumulate(begin, end, 0.0);
 		}
 
-        /**
-         * This method calculates the mean of a given range.
-         *
-         * @param begin InputIterator
+		/**
+		 * This method calculates the mean of a given range.
+		 *
+		 * @param begin InputIterator
 		 * @param end InputIterator
-         * @return Mean of the given range
-         */
-        template <typename value_type, typename InputIterator>
-        double mean(InputIterator begin, InputIterator end) {
-            return (std::accumulate(begin, end,  0.0) / ((value_type)std::distance(begin,end)));
-        }
+		 * @return Mean of the given range
+		 */
+		template <typename value_type, typename InputIterator>
+		double mean(InputIterator begin, InputIterator end)
+		{
+			return (std::accumulate(begin, end, 0.0) /
+			        ((value_type)std::distance(begin, end)));
+		}
 
 		/**
 		 * This method calculates the max-mean statistic of a given range.
@@ -152,7 +177,8 @@ namespace GeneTrail {
 		 * @return Max-mean statistic of the given range
 		 */
 		template <typename value_type, typename InputIterator>
-		value_type max_mean(InputIterator begin, InputIterator end){
+		value_type max_mean(InputIterator begin, InputIterator end)
+		{
 			value_type positive_sum = 0;
 			value_type negative_sum = 0;
 			for(auto iter = begin; iter != end; ++iter) {
@@ -162,16 +188,16 @@ namespace GeneTrail {
 					positive_sum += *iter;
 				}
 			}
-			value_type n = std::distance(begin,end);
+			value_type n = std::distance(begin, end);
 			// In the original paper, they return absolute values.
 			// But as we want to distinguish between positive and negative,
 			// we return signed values.
-			if(negative_sum > positive_sum){
+			if(negative_sum > positive_sum) {
 				return -negative_sum / n;
-			}else{
+			} else {
 				return positive_sum / n;
 			}
-			//return std::max(negative_sum / n, positive_sum / n);
+			// return std::max(negative_sum / n, positive_sum / n);
 		}
 
 		/**
@@ -209,41 +235,44 @@ namespace GeneTrail {
 		 *
 		 * @param begin InputIterator
 		 * @param end InputIterator
-         * @return Variance of the given range
-         */
-        template <typename value_type, typename InputIterator>
-        value_type var(InputIterator begin, InputIterator end) {
-            int n = std::distance(begin,end);
-            if (n == 1) {
-                return ((value_type) 0);
-            } else {
-                value_type s = std::accumulate(begin, end, ((value_type) 0));
-                value_type mean = s / n;
-                value_type v = (value_type) 0;
-				value_type ep = (value_type) 0;
-                for (auto it = begin; it != end; ++it) {
-                    s = *it - mean;
-                    ep += s;
-                    v += s*s;
-                }
-                return (v - ep * ep / n) / (n - 1);
-            }
-        }
-
-        /**
-         * This method calculates the standard deviation of a given range.
-         *
-         * @param begin InputIterator
-		 * @param end InputIterator
-         * @return Standard deviation of the given range
-         */
-        template <typename value_type, typename InputIterator>
-        value_type sd(InputIterator begin, InputIterator end) {
-            return std::sqrt(var<value_type,InputIterator>(begin,end));
-        }
+		 * @return Variance of the given range
+		 */
+		template <typename value_type, typename InputIterator>
+		value_type var(InputIterator begin, InputIterator end)
+		{
+			int n = std::distance(begin, end);
+			if(n == 1) {
+				return ((value_type)0);
+			} else {
+				value_type s = std::accumulate(begin, end, ((value_type)0));
+				value_type mean = s / n;
+				value_type v = (value_type)0;
+				value_type ep = (value_type)0;
+				for(auto it = begin; it != end; ++it) {
+					s = *it - mean;
+					ep += s;
+					v += s * s;
+				}
+				return (v - ep * ep / n) / (n - 1);
+			}
+		}
 
 		/**
-		 * This method calculates the sample covariance between two ranges of values.
+		 * This method calculates the standard deviation of a given range.
+		 *
+		 * @param begin InputIterator
+		 * @param end InputIterator
+		 * @return Standard deviation of the given range
+		 */
+		template <typename value_type, typename InputIterator>
+		value_type sd(InputIterator begin, InputIterator end)
+		{
+			return std::sqrt(var<value_type, InputIterator>(begin, end));
+		}
+
+		/**
+		 * This method calculates the sample covariance between two ranges of
+		 *values.
 		 *
 		 * @param first_begin InputIterator
 		 * @param first_end InputIterator
@@ -252,14 +281,20 @@ namespace GeneTrail {
 		 * @return Covariance of the given range
 		 */
 		template <typename value_type, typename InputIterator>
-		value_type cov(InputIterator first_begin, InputIterator first_end, InputIterator second_begin, InputIterator second_end){
-			assert(std::distance(first_begin, first_end) == std::distance(second_begin, second_end));
-			value_type mean1 = mean<value_type,InputIterator>(first_begin, first_end);
-			value_type mean2 = mean<value_type,InputIterator>(second_begin, second_end);
+		value_type cov(InputIterator first_begin, InputIterator first_end,
+		               InputIterator second_begin, InputIterator second_end)
+		{
+			assert(std::distance(first_begin, first_end) ==
+			       std::distance(second_begin, second_end));
+			value_type mean1 =
+			    mean<value_type, InputIterator>(first_begin, first_end);
+			value_type mean2 =
+			    mean<value_type, InputIterator>(second_begin, second_end);
 			value_type cov = 0.0;
 			size_t n = std::distance(first_begin, first_end);
 			for(size_t i = 0; i < n; ++i) {
-				cov += (*(first_begin + i) - mean1)*(*(second_begin + i) - mean2);
+				cov += (*(first_begin + i) - mean1) *
+				       (*(second_begin + i) - mean2);
 			}
 			return cov / (n - 1);
 		}
@@ -274,26 +309,33 @@ namespace GeneTrail {
 		 * @return Correlation coefficient
 		 */
 		template <typename value_type, typename InputIterator>
-		value_type pearson_correlation(InputIterator first_begin, InputIterator first_end, InputIterator second_begin, InputIterator second_end){
-			assert(std::distance(first_begin, first_end) == std::distance(second_begin, second_end));
-			value_type covar = cov<value_type,InputIterator>(first_begin, first_end, second_begin, second_end);
-			value_type sd1 = sd<value_type,InputIterator>(first_begin, first_end);
-			value_type sd2 = sd<value_type,InputIterator>(second_begin, second_end);
+		value_type pearson_correlation(InputIterator first_begin,
+		                               InputIterator first_end,
+		                               InputIterator second_begin,
+		                               InputIterator second_end)
+		{
+			assert(std::distance(first_begin, first_end) ==
+			       std::distance(second_begin, second_end));
+			value_type covar = cov<value_type, InputIterator>(
+			    first_begin, first_end, second_begin, second_end);
+			value_type sd1 =
+			    sd<value_type, InputIterator>(first_begin, first_end);
+			value_type sd2 =
+			    sd<value_type, InputIterator>(second_begin, second_end);
 			return covar / (sd1 * sd2);
 		}
 
 		/**
 		 */
 		template <typename value_type, typename InputIterator>
-		std::vector<int> ranks(InputIterator begin, InputIterator end){
+		std::vector<int> ranks(InputIterator begin, InputIterator end)
+		{
 			std::vector<value_type> tmp(begin, end);
 			std::sort(tmp.begin(), tmp.end());
 			std::vector<int> ranks;
-			for(auto it = begin; it!= end; ++it)
-			{
+			for(auto it = begin; it != end; ++it) {
 				for(size_t i = 0; i < tmp.size(); ++i) {
-					if(*it == *(tmp.begin()+i))
-					{
+					if(*it == *(tmp.begin() + i)) {
 						ranks.push_back(i);
 					}
 				}
@@ -311,10 +353,18 @@ namespace GeneTrail {
 		 * @return Correlation coefficient
 		 */
 		template <typename value_type, typename InputIterator>
-		value_type spearman_correlation(InputIterator first_begin, InputIterator first_end, InputIterator second_begin, InputIterator second_end){
-			std::vector<int> first_ranks = ranks<value_type, InputIterator>(first_begin, first_end);
-			std::vector<int> second_ranks = ranks<value_type, InputIterator>(second_begin, second_end);
-			return pearson_correlation<value_type,std::vector<int>::iterator>(first_ranks.begin(), first_ranks.end(), second_ranks.begin(), second_ranks.end());
+		value_type spearman_correlation(InputIterator first_begin,
+		                                InputIterator first_end,
+		                                InputIterator second_begin,
+		                                InputIterator second_end)
+		{
+			std::vector<int> first_ranks =
+			    ranks<value_type, InputIterator>(first_begin, first_end);
+			std::vector<int> second_ranks =
+			    ranks<value_type, InputIterator>(second_begin, second_end);
+			return pearson_correlation<value_type, std::vector<int>::iterator>(
+			    first_ranks.begin(), first_ranks.end(), second_ranks.begin(),
+			    second_ranks.end());
 		}
 
 		/**
@@ -333,7 +383,8 @@ namespace GeneTrail {
 		log_mean_fold_quotient(InputIterator1 begin1, InputIterator1 end1,
 		                       InputIterator2 begin2, InputIterator2 end2)
 		{
-			return std::log(mean<value_type, InputIterator1>(begin1,end1)) - std::log(mean<value_type, InputIterator2>(begin2, end2));
+			return std::log(mean<value_type, InputIterator1>(begin1, end1)) -
+			       std::log(mean<value_type, InputIterator2>(begin2, end2));
 		}
 
 		/**
@@ -350,9 +401,30 @@ namespace GeneTrail {
 		          typename InputIterator2>
 		value_type
 		mean_fold_quotient(InputIterator1 begin1, InputIterator1 end1,
-		                       InputIterator2 begin2, InputIterator2 end2)
+		                   InputIterator2 begin2, InputIterator2 end2)
 		{
-			return mean<value_type, InputIterator1>(begin1, end1) /  mean<value_type, InputIterator2>(begin2, end2);
+			return mean<value_type, InputIterator1>(begin1, end1) /
+			       mean<value_type, InputIterator2>(begin2, end2);
+		}
+
+		/**
+		* This function calculates mean(a) -  mean(b).
+		* Mean-Fold-Difference
+		*
+		* @param begin1
+		* @param end1
+		* @param begin2
+		* @param end2
+		* @return mean(a) -  mean(b)
+		*/
+		template <typename value_type, typename InputIterator1,
+		          typename InputIterator2>
+		value_type
+		mean_fold_difference(InputIterator1 begin1, InputIterator1 end1,
+		                     InputIterator2 begin2, InputIterator2 end2)
+		{
+			return mean<value_type, InputIterator1>(begin1, end1) -
+			       mean<value_type, InputIterator2>(begin2, end2);
 		}
 
 		/**
@@ -364,9 +436,11 @@ namespace GeneTrail {
 		 * @return Z-score
 		 */
 		template <typename value_type, typename InputIterator1>
-		value_type z_score(value_type x, InputIterator1 begin, InputIterator1 end)
+		value_type z_score(value_type x, InputIterator1 begin,
+		                   InputIterator1 end)
 		{
-			return (x - mean<value_type, InputIterator1>(begin, end))/sd<value_type, InputIterator1>(begin, end);
+			return (x - mean<value_type, InputIterator1>(begin, end)) /
+			       sd<value_type, InputIterator1>(begin, end);
 		}
 	}
 }
