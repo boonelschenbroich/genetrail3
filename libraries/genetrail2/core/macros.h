53262fed18322c73b26e2f96edfe09c072582fca
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2014-12-03 18:02:10 +0100

    [core] Fix macros.h include guard

--- libraries/genetrail2/core/macros.h
+++ libraries/genetrail2/core/macros.h
@@ -1,8 +1,8 @@
-#ifndef GENETRAIL2_CONFIG_CONFIG_H
-#define GENETRAIL2_CONFIG_CONFIG_H
+#ifndef GENETRAIL2_CONFIG_MACROS_H
+#define GENETRAIL2_CONFIG_MACROS_H
 
 #define GT2_EXPORT          __attribute__((visibility ("default")))
 #define GT2_LOCAL           __attribute__((visibility ("hidden")))
 #define GT2_EXTERN_VARIABLE extern __attribute__((visibility ("default")))
 
-#endif //GENETRAIL2_CONFIG_CONFIG_H
+#endif //GENETRAIL2_CONFIG_MACROS_H
