fc69e76e1a7d4a2c26fdf244fc0b88ad50ae2d4e
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2014-12-29 18:18:14 +0100

    [doc] Improve documentation of the File base classes.

--- libraries/genetrail2/core/File.h
+++ libraries/genetrail2/core/File.h
@@ -15,17 +15,41 @@ namespace GeneTrail
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wattributes"
 
+	/**
+	 * An enum that specifies for which purpose a file should be opened.
+	 * Currently only reading, writing, and appending is supported.
+	 */
 	enum class GT2_EXPORT FileOpenMode {
+		/// Open the file in read mode.
 		READ,
+		/// Open the file in write mode.
 		WRITE,
+		/// Open the file in append mode.
 		APPEND
 	};
 
 #pragma GCC diagnostic pop
 
+	/**
+	 * This is a base class that provides some convenience features like
+	 * transparent compression/decompression.
+	 *
+	 * It should not be used directly, but rather as a base class
+	 * from which to create custom parsers/serializers/deserializers.
+	 *
+	 * The class is not copyable, but assignable.
+	 */
 	class GT2_EXPORT GenericFile
 	{
 		public:
+		/**
+		 * Default constructor. Opens the file at the specified path
+		 * using the specified OpenMode.
+		 *
+		 * @param path The file which should be opened.
+		 * @param mode Specifies whether the file should be opened for reading or
+		 *             writing.
+		 */
 		GenericFile(const std::string& path, FileOpenMode mode);
 
 		GenericFile(const GenericFile&) = delete;
@@ -34,14 +58,45 @@ namespace GeneTrail
 		GenericFile(GenericFile&&) = default;
 		GenericFile& operator=(GenericFile&&) = default;
 
+		/**
+		 * Virtual destructor.
+		 */
 		virtual ~GenericFile();
 
+		/**
+		 * Bool conversion operator
+		 *
+		 * @returns true when the underlying stream is in a good state.
+		 */
 		operator bool() const;
+
+		/**
+		 * Logical not operator
+		 *
+		 * @returns true when the underlying stream is in a bad state.
+		 */
 		bool operator!() const;
 
+		/**
+		 * Check the current open mode.
+		 *
+		 * @returns true iff the file was opened using OpenMode::READ
+		 */
 		bool isReading() const;
+
+		/**
+		 * Check the current open mode.
+		 *
+		 * @returns true iff the file was opened using
+		 *          OpenMode::WRITE || OpenMode::APPEND.
+		 */
 		bool isWriting() const;
 
+		/**
+		 * Get the OpenMode with which the file was opened.
+		 *
+		 * @returns The OpenMode with which the file was opened.
+		 */
 		FileOpenMode mode() const;
 
 		protected:
@@ -69,6 +124,11 @@ namespace GeneTrail
 		void openForWriting_(const std::string& path, std::ios_base::openmode m);
 	};
 
+	/**
+	 * An abstract base class for (de-)serializers. It provides a default
+	 * input/output operator implementation as well as a basic interface
+	 * for reading and writing.
+	 */
 	template <typename ReturnType> class File : public GenericFile
 	{
 		public:
@@ -76,11 +136,29 @@ namespace GeneTrail
 
 		virtual ~File() {};
 
+		/**
+		 * Read a ReturnType instance from the stream and return it.
+		 *
+		 * @returns A valid ReturnType instance.
+		 */
 		virtual ReturnType read() = 0;
+
+		/**
+		 * Writes a ReturnType instance to the stream.
+		 *
+		 * @returns true iff writing was completed sucessfully.
+		 */
 		virtual bool write(const ReturnType& r) = 0;
 
+		/**
+		 * Global output operator.
+		 */
 		template<typename R>
 		friend File<R>& operator<<(File<R>&, const R&);
+
+		/**
+		 * Global input operator.
+		 */
 		template<typename R>
 		friend File<R>& operator>>(File<R>&, R&);
 	};
