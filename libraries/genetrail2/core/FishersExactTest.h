ec04b7a21cc1eede9ab59909283c0f862003efef
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2015-01-04 21:16:18 +0100

    [core] FishersExactTest: Formatting

--- libraries/genetrail2/core/FishersExactTest.h
+++ libraries/genetrail2/core/FishersExactTest.h
@@ -26,68 +26,95 @@
 
 #include <iostream>
 
-namespace GeneTrail {
+namespace GeneTrail
+{
 
-    /**
-     * Fisher's exact test
-     */
-    template <typename unsigned_integer_type = unsigned int, typename return_type = double >
-    class GT2_EXPORT FishersExactTest {
-    public:
+	/**
+	 * Fisher's exact test
+	 */
+	template <typename unsigned_integer_type = unsigned int,
+	          typename return_type = double>
+	class GT2_EXPORT FishersExactTest
+	{
+		public:
+		/**
+		 * This method implements the Fisher's exact test.
+		 *
+		 * @param m Number of genes in the reference set (population size)
+		 * @param l Number of genes in category (success states in the
+		 *          population)
+		 * @param n Number of genes in test set (Number of draws)
+		 * @param k Number of genes in the test set that belong to the category
+		 *          (Number of successes)
+		 * @param i
+		 *
+		 * @return
+		 */
+		return_type compute(const unsigned_integer_type& m,
+		                    const unsigned_integer_type& l,
+		                    const unsigned_integer_type& n,
+		                    const unsigned_integer_type& k,
+		                    const unsigned_integer_type& i)
+		{
+			return (boost::math::binomial_coefficient<return_type>(n, i) *
+			        boost::math::binomial_coefficient<return_type>(m, l + k - i)) /
+			       boost::math::binomial_coefficient<return_type>(m + n, l + k);
+		}
 
-        /**
-         * This method implements the Fisher's exact test.
-         *
-         * @param m Number of genes in the reference set (population size)
-         * @param l Number of genes in category (success states in the population)
-         * @param n Number of genes in test set (Number of draws)
-         * @param k Number of genes in the test set that belong to the category (Number of successes)
-         * @param i
-         *
-         * @return
-         */
-        return_type compute(const unsigned_integer_type& m, const unsigned_integer_type& l, const unsigned_integer_type& n, const unsigned_integer_type& k, const unsigned_integer_type& i) {
-            return (boost::math::binomial_coefficient<return_type>(n, i) * boost::math::binomial_coefficient<return_type>(m, l + k - i)) / boost::math::binomial_coefficient<return_type>(m + n, l + k);
-        }
-
-        /**
-         * This method computes the lower-tailed p-value for the given event of the Fisher's exact test.
-         *
-         * @param m Number of genes in the reference set (population size)
-         * @param l Number of genes in category (success states in the population)
-         * @param n Number of genes in test set (Number of draws)
-         * @param k Number of genes in the test set that belong to the category (Number of successes)
-         * @param i
-         *
-         * @return
-         */
-        return_type lowerTailedPValue(const unsigned_integer_type& m, const unsigned_integer_type& l, const unsigned_integer_type& n, const unsigned_integer_type& k) {
+		/**
+		 * This method computes the lower-tailed p-value for the given event of
+		 *the Fisher's exact test.
+		 *
+		 * @param m Number of genes in the reference set (population size)
+		 * @param l Number of genes in category (success states in the
+		 *          population)
+		 * @param n Number of genes in test set (Number of draws)
+		 * @param k Number of genes in the test set that belong to the category
+		 *          (Number of successes)
+		 * @param i
+		 *
+		 * @return
+		 */
+		return_type lowerTailedPValue(const unsigned_integer_type& m,
+		                              const unsigned_integer_type& l,
+		                              const unsigned_integer_type& n,
+		                              const unsigned_integer_type& k)
+		{
 			return_type p = 0;
-            for (unsigned_integer_type i = 0; i <= k; ++i) {
-                p += boost::math::binomial_coefficient<return_type>(n, i) * boost::math::binomial_coefficient<return_type>(m, l + k - i);
-            }
-            return p / boost::math::binomial_coefficient<return_type>(m + n, l + k);
-        }
+			for(unsigned_integer_type i = 0; i <= k; ++i) {
+				p += boost::math::binomial_coefficient<return_type>(n, i) *
+				     boost::math::binomial_coefficient<return_type>(m, l + k - i);
+			}
+			return p / boost::math::binomial_coefficient<return_type>(m + n, l + k);
+		}
 
-        /**
-         * This method computes the upper-tailed p-value for the given event of the Fisher's exact test.
-         *
-         * @param m Number of genes in the reference set (population size)
-         * @param l Number of genes in category (success states in the population)
-         * @param n Number of genes in test set (Number of draws)
-         * @param k Number of genes in the test set that belong to the category (Number of successes)
-         *
-         * @return
-         */
-        return_type upperTailedPValue(const unsigned_integer_type& m, const unsigned_integer_type& l, const unsigned_integer_type& n, const unsigned_integer_type& k) {
+		/**
+		 * This method computes the upper-tailed p-value for the given event of
+		 *the Fisher's exact test.
+		 *
+		 * @param m Number of genes in the reference set (population size)
+		 * @param l Number of genes in category (success states in the
+		 *          population)
+		 * @param n Number of genes in test set (Number of draws)
+		 * @param k Number of genes in the test set that belong to the category
+		 *          (Number of successes)
+		 *
+		 * @return
+		 */
+		return_type upperTailedPValue(const unsigned_integer_type& m,
+		                              const unsigned_integer_type& l,
+		                              const unsigned_integer_type& n,
+		                              const unsigned_integer_type& k)
+		{
 			return_type p = 0;
 			unsigned_integer_type d = std::min(n, l + k);
-			for (unsigned_integer_type i = k; i <= d; ++i) {
-            	p += boost::math::binomial_coefficient<return_type>(n, i) * boost::math::binomial_coefficient<return_type>(m, l + k - i);
+			for(unsigned_integer_type i = k; i <= d; ++i) {
+				p += boost::math::binomial_coefficient<return_type>(n, i) *
+				     boost::math::binomial_coefficient<return_type>(m, l + k - i);
 			}
-            return p/boost::math::binomial_coefficient<return_type>(m + n, l + k);
-        }
-    };
+			return p / boost::math::binomial_coefficient<return_type>(m + n, l + k);
+		}
+	};
 }
 
 #endif // GT2_CORE_FISHERS_EXACT_TEST_H
