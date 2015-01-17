597d8dd40d08da11f068be5d2e1fc56eee93ee22
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2015-01-17 22:01:39 +0100

    [cmake] Add a runtime check for std::make_unique

--- cmake/GT2ConfigHeader.cmake
+++ cmake/GT2ConfigHeader.cmake
@@ -1,1 +1,10 @@
+include(CheckCXXSourceCompiles)
+
+SET(CMAKE_REQUIRED_FLAGS ${CXX_FLAGS})
+check_cxx_source_compiles(
+"
+#include <memory>
+int main(){auto a = std::make_unique<int>();}
+" GENETRAIL2_HAS_MAKE_UNIQUE)
+
 configure_file(cmake/config.h.in "${PROJECT_BINARY_DIR}/libraries/genetrail2/core/config.h")
