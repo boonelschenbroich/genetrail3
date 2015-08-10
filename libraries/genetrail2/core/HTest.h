cfd997cab9b2f8aa1b893cb830a9a6dc0bfcc7e0
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2015-08-10 12:55:40 +0200

    [core] htests: simplify the template declarations
    We now use the typedef exported by the used test.

--- libraries/genetrail2/core/HTest.h
+++ libraries/genetrail2/core/HTest.h
@@ -7,37 +7,54 @@ namespace GeneTrail
 {
 	namespace HTest
 	{
-		template<template<typename T> class HTest, typename value_type, typename InputIterator1, typename InputIterator2>
-		value_type test(HTest<value_type>& t, const InputIterator1& first_begin, const InputIterator1& first_end, const InputIterator2& second_begin, const InputIterator2& second_end){
+		template <typename Test, typename InputIterator1,
+		          typename InputIterator2>
+		typename Test::value_type test(Test& t,
+		                               const InputIterator1& first_begin,
+		                               const InputIterator1& first_end,
+		                               const InputIterator2& second_begin,
+		                               const InputIterator2& second_end)
+		{
 			return t.test(first_begin, first_end, second_begin, second_end);
 		}
 
-		template<template<typename T> class HTest, typename value_type>
-		value_type twoSidedPValue(HTest<value_type>& t, const value_type& score) {
-			return 2.0 * boost::math::cdf(boost::math::complement(t.distribution(), fabs(score)));
-		}
-
-		template<template<typename T> class HTest, typename value_type>
-		std::pair<value_type, value_type> confidenceInterval(HTest<value_type>& t, const value_type& alpha){
-			return t.confidenceInterval(alpha);
-		}
-
-		template<template<typename T> class HTest, typename value_type, typename InputIterator>
-		value_type test(HTest<value_type>& t, const InputIterator& begin, const InputIterator& end){
+		template <typename Test, typename InputIterator>
+		typename Test::value_type test(Test& t, const InputIterator& begin,
+		                               const InputIterator& end)
+		{
 			return t.test(begin, end);
 		}
 
-		template<template<typename T> class HTest, typename value_type>
-		value_type lowerTailedPValue(HTest<value_type>& t, const value_type& score) {
+		template <typename Test>
+		typename Test::value_type
+		twoSidedPValue(Test& t, const typename Test::value_type& score)
+		{
+			return 2.0 * boost::math::cdf(boost::math::complement(
+			                 t.distribution(), fabs(score)));
+		}
+
+		template <typename Test>
+		std::pair<typename Test::value_type, typename Test::value_type>
+		confidenceInterval(Test& t, const typename Test::value_type& alpha)
+		{
+			return t.confidenceInterval(alpha);
+		}
+
+		template <typename Test>
+		typename Test::value_type
+		lowerTailedPValue(Test& t, const typename Test::value_type& score)
+		{
 			return boost::math::cdf(t.distribution(), score);
 		}
 
-		template<template<typename T> class HTest, typename value_type>
-		value_type upperTailedPValue(HTest<value_type>& t, const value_type& score) {
-			return boost::math::cdf(boost::math::complement(t.distribution(), score));
+		template <typename Test>
+		typename Test::value_type
+		upperTailedPValue(Test& t, const typename Test::value_type& score)
+		{
+			return boost::math::cdf(
+			    boost::math::complement(t.distribution(), score));
 		}
 	}
 }
 
-#endif //GT2_CORE_HTEST_H
-
+#endif // GT2_CORE_HTEST_H
