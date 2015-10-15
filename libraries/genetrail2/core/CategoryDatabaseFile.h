442d9cdedcbcdab4b9bab16cac01279813dbe16b
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2015-10-15 18:28:02 +0200

    [core] Add a CategoryDatabaseFile typedef

---
+++ libraries/genetrail2/core/CategoryDatabaseFile.h
@@ -0,0 +1,35 @@
+/*
+ * GeneTrail2 - An efficent library for interpreting genetic data
+ * Copyright (C) 2016 Daniel Stöckel <dstoeckel@bioinf.uni-sb.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef GT2_CORE_CATEGORYDATABASEFILE_H
+#define GT2_CORE_CATEGORYDATABASEFILE_H
+
+#include "CategoryDatabase.h"
+#include "File.h"
+
+#include "macros.h"
+
+namespace GeneTrail
+{
+	using CategoryDatabaseFile = File<CategoryDatabase>;
+}
+
+#endif //GT2_CORE_CATEGORYDATABASEFILE_H
+
