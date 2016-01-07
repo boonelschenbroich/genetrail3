47c01b0f29fcacb99b44bfa01409e36c4ec03d69
Daniel Stöckel <dstoeckel@bioinf.uni-sb.de>
2016-01-08 00:36:38 +0100

    [core] Remove lots of unneeded includes
    Especially the adjacency_matrix include caused a compile error due to a bug in boost.

--- libraries/genetrail2/core/BoostGraph.h
+++ libraries/genetrail2/core/BoostGraph.h
@@ -20,53 +20,8 @@
 #ifndef GT2_CORE_GRAPH_H
 #define GT2_CORE_GRAPH_H
 
-#ifndef BOOST_GRAPH_UTILITY_HPP
-#include <boost/graph/graph_utility.hpp>
-#endif
-
-#ifndef BOOST_GRAPH_GRAPH_SEARCH_VISITORS_HPP
-#include <boost/graph/visitors.hpp>
-#endif
-
-#ifndef BOOST_GRAPH_BREADTH_FIRST_SEARCH_HPP
-#include <boost/graph/breadth_first_search.hpp>
-#endif
-
-#ifndef BOOST_PROPERTY_MAP_HPP
-#include <boost/property_map.hpp>
-#endif
-
-#ifndef BOOST_GRAPH_CONNECTED_COMPONENTS_HPP
-#include <boost/graph/connected_components.hpp>
-#endif
-
-#ifndef BOOST_GRAPH_STRONG_COMPONENTS_HPP
-#include <boost/graph/strong_components.hpp>
-#endif
-
-#ifndef BOOST_GRAPH_ADJACENCY_LIST_HPP
 #include <boost/graph/adjacency_list.hpp>
-#endif
-
-#ifndef BOOST_GRAPH_ADJACENCY_MATRIX_HPP
-#include <boost/graph/adjacency_matrix.hpp>
-#endif
-
-#ifndef BOOST_GRAPH_TRAITS_HPP
 #include <boost/graph/graph_traits.hpp>
-#endif
-
-#ifndef BOOST_FILTERED_GRAPH_HPP
-#include <boost/graph/filtered_graph.hpp>
-#endif
-
-#ifndef REVERSE_GRAPH_DWA092300_H_
-#include <boost/graph/reverse_graph.hpp>
-#endif
-
-#ifndef BOOST_GRAPH_JOHNSON_HPP
-#include <boost/graph/johnson_all_pairs_shortest.hpp>
-#endif
 
 #include <string>
 
