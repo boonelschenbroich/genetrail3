de1d9169d0225ca8c783c3e9890313f7edba0bc4
Daniel Stöckel <dstoeckel@bioinf.uni-sb.de>
2016-01-07 23:56:04 +0100

    [enrichment] Added missing license headers

--- libraries/genetrail2/enrichment/CommandLineInterface.cpp
+++ libraries/genetrail2/enrichment/CommandLineInterface.cpp
@@ -1,3 +1,23 @@
+/*
+ * GeneTrail2 - An efficent library for interpreting genetic data
+ * Copyright (C) 2015 Daniel Stöckel <dstoeckel@bioinf.uni-sb.de>
+ *               2014 Tim Kehl <tkehl@bioinf.uni-sb.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 #include "CommandLineInterface.h"
 
 #include "Parameters.h"
