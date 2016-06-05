b9ed636213b75476539849342406044b9ffe6216
Daniel Stöckel <dstoeckel@bioinf.uni-sb.de>
2016-06-05 13:09:24 +0200

    [applications] Make integration_tests_worker more reliable
    Fix some lints suggested by shellcheck

--- applications/test/enrichment/integration_tests_worker.sh
+++ applications/test/enrichment/integration_tests_worker.sh
@@ -1,4 +1,6 @@
-source $1
+#!/bin/bash
+
+source "$1"
 
 RANDOM_SEED=8095254980
 ROW_PERMUTATIONS=50000
@@ -17,9 +19,9 @@ function runScoreMethodColwise {
 		--scores "${BASE_DIR}/input/scores_colwise.txt" \
 		--pvalue_strategy column-wise \
 		--scoring_method independent-shrinkage-t-test \
-		--groups ${BASE_DIR}/input/groups.txt \
-		--data_matrix_path ${BASE_DIR}/input/data.txt \
-		--permutations ${COL_PERMUTATIONS}
+		--groups "${BASE_DIR}/input/groups.txt" \
+		--data_matrix_path "${BASE_DIR}/input/data.txt" \
+		--permutations "${COL_PERMUTATIONS}"
 }
 
 function runMethod {
@@ -29,7 +31,7 @@ function runMethod {
 
 	echo -n "Running ${ID}... "
 
-	"${BINARY_PATH}/$1" $3 --seed ${RANDOM_SEED} --categories "${GENERATED_INPUT_DIR}/categories.txt" -o "${OUTPUT}" ${@:4} > /dev/null
+	"${BINARY_PATH}/$1" $3 --seed "${RANDOM_SEED}" --categories "${GENERATED_INPUT_DIR}/categories.txt" -o "${OUTPUT}" "${@:4}" > /dev/null
 
 	if [[ ! -f "${OUTPUT}/KEGG.txt" ]]; then
 		echo "[1;31mNo output has been produced![0m"
@@ -47,7 +49,7 @@ function runMethod {
 		return
 	fi
 
-	local DIFF=`diff -purN "${REFERENCE}" "${OUTPUT}${ID}.txt"`
+	local DIFF=$(diff -purN "${REFERENCE}" "${OUTPUT}${ID}.txt")
 
 	if [[ "${DIFF}" != '' ]]; then
 		echo "[1;31mError[0m"
@@ -65,7 +67,7 @@ function runMethod {
 
 ## enrichment
 methods=( sum mean median max-mean )
-for i in ${methods[@]}
+for i in "${methods[@]}"
 do
 	runScoreMethod enrichment "${i}_rowwise" "--method $i --permutations ${ROW_PERMUTATIONS}"
 	runScoreMethodColwise enrichment "${i}_colwise" "--method $i"
@@ -79,7 +81,7 @@ runScoreMethod weighted-gsea rowwise "--permutations ${ROW_PERMUTATIONS}"
 runScoreMethodColwise weighted-gsea colwwise
 
 methods=( 'one-sample-t-test' 'two-sample-t-test' 'two-sample-wilcoxon' )
-for i in ${methods[@]}
+for i in "${methods[@]}"
 do
 	runScoreMethod htests "${i}_rowwise" "--method $i"
 	runScoreMethodColwise htests "${i}_colwise" "--method $i"
