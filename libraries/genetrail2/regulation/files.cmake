f374990093986e4694d3a3382789acc53fa58435
Tim Kehl <Tim.Kehl@bioinf.uni-sb.de>
2016-06-29 17:09:25 +0200

    Added files.cmake for regulation library.

---
+++ libraries/genetrail2/regulation/files.cmake
@@ -0,0 +1,20 @@
+
+####################################################################################################
+# Path to all files in this directory
+####################################################################################################
+
+SET(DIR ${PROJECT_SOURCE_DIR})
+SET(LIBNAME "REGULATION")
+
+####################################################################################################
+# All files of this directory
+####################################################################################################
+
+# We list all files separatly to be able to leave some out, if we do not want to compile them.
+
+add_header_to_library(RegulationFileParser.h)
+add_header_to_library(RegulationBootstrapper.h)
+add_header_to_library(RegulatorGeneAssociationEnrichmentResult.h)
+add_header_to_library(RegulatorGeneAssociationEnrichmentAnalysis.h)
+add_header_to_library(RegulatorGeneAssociationEnrichmentAlgorithms.h)
+add_header_to_library(RegulatorImpactScore.h)
